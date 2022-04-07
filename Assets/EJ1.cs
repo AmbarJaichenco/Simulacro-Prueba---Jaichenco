6e72eaa2720e3960523838cfed333fe77983d628
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2022-04-07 10:02:47 -0300

    Initial commit

---
+++ Assets/EJ1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ1 : MonoBehaviour
+{
+    // 1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EJ2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    // Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible. 
+    //Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuánto dinero sobra o falta.
+
+    public float pr1;
+    public float pr2;
+    public float pr3;
+    public float dindisp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float sumapr = pr1 + pr2 + pr3;
+        if (sumapr > dindisp)
+        {
+            Debug.Log("No hay suficiente dinero disponible");
+            float falta = sumapr - dindisp;
+            Debug.Log("Faltan $" + falta);
+        }
+        else
+        {
+            Debug.Log("Hay suficiente dinero disponible");
+            float sobra = dindisp - sumapr;
+            Debug.Log("Sobran $" + sobra);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
